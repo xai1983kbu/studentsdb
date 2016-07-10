3f23e857abacad8ac6b94d09de45b116dd187023
Andriy Sorochan <xai1983kbu@gmail.com>
2016-07-10 20:43:13 +0300

    read book on page 184

--- students/views.py
+++ students/views.py
@@ -6,7 +6,25 @@ from django.http import HttpResponse
 # Views for Students
 
 def students_list(request):
-    return render(request,'students/students_list.html',{})
+    students = (
+        {'id': 1,
+         'first_name': 'Андрій',
+         'last_name': 'Сорочан',
+         'ticket': 2123,
+         'image': 'img/my_photo.jpg'},
+        {'id': 2,
+         'first_name': 'Андрій2',
+         'last_name': 'Сорочан2',
+         'ticket': 21232,
+         'image': 'img/my_photo2.jpg'},
+        {'id': 3,
+         'first_name': 'Андрій3',
+         'last_name': 'Сорочан3',
+         'ticket': 21233,
+         'image': 'img/my_photo3.jpg'},
+)
+
+    return render(request,'students/students_list.html',{'students':students})
 
 def students_add(reduest):
     return HttpResponse('<h1>Student Add Form</h1>')
