58c07f07aa0d0e672559008330082a3aebbae925
Andriy Sorochan <xai1983kbu@gmail.com>
2016-07-10 15:43:07 +0300

    read book on page 173

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -1,21 +1,19 @@
-"""studentsdb URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = [
+    # Students urls
+    url(r'^$','students.views.students_list', name='home'),
+    url(r'^students/add/?$','students.views.students_add', name='students_add'),
+    url(r'^students/(?P<sid>[0-9]+)/edit/?$','students.views.students_edit', name='students_edit'),
+    url(r'^students/(?P<sid>[0-9]+)/delete/?$','students.views.students_delete', name='students_delete'),
+    
+    # Groups urls
+    url(r'^mygroups/$', 'students.views.groups_list', name='groups'),
+    url(r'^groups/add/?$','students.views.groups_add', name='groups_add'),
+    url(r'^groups/(?P<gid>[0-9]+)/edit/?$','students.views.groups_edit', name='groups_edit'),
+    url(r'^groups/(?P<gid>[0-9]+)/delete/?$','students.views.groups_delete', name='groups_delete'),
+
+
     url(r'^admin/', admin.site.urls),
 ]
