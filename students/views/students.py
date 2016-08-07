6c556205c124c69a4fb857ebc5498828f8bea1e2
Andriy Sorochan <xai1983kbu@gmail.com>
2016-08-07 13:19:05 +0300

    1. Додав медіа шаблон до urls.py модуля if DEBUG: urlpatterns += patterns('',     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {          'document_root': MEDIA_ROOT}))
    2. Налаштував та вдосконалив сортування студентів за прізвищем, ім'ям та білетом
    Дивись файли: students.py (#try to order students list)
    Дивись файли: students_list.html pagination.html
    
    3. Налаштував та вдосконалив навігацію по списку студентів.
    Дивись файли: students.py (#paginate students)
    Дивись файли: students_list.html pagination.html
    
    4. Виконав домашне завдання по 'первиному' сортуванню студентів по прізвищю
       1 Варіант: додати ordering = ['last_name'] в клас мета класу студент, models.py
    
       class Student(models.Model):
            """Student Model"""
    
            class Meta(object):
                verbose_name = "Студент"
                verbose_name_plural = "Студенти"
                ordering = ['last_name']
    
       2 Варіант: додати перед '#try to order students list' наступний код
    
        if request.get_full_path() == "/":
            #redirect request.GET on its copy(deep copy) which I will amend
            request.GET = request.GET.copy()
            #assign 'order_by' value 'last_name'
            request.GET.__setitem__('order_by', 'last_name')
    
    5. Виконав(частково) домашне завдання на сторінці 256, але трохи по своєму
    і поки що без техноголії Ajax.
    """Трохи забігаючи наперед пропоную самостійно спробувати розібратись із
    Javascript та технологією Ajax, і спробувати реалізувати навігацію з допомо-
    гою кнопки “Load More...” (Завантажити Більше). Думаю ви вже неодноразо-
    во бачили такий тип навігації, коли доскролюєте до кінця списку елементів,
    а там внизу бачите кнопку “Load More...”, при кліку по якій, динамічно
    довантажується наступна пачка елементів. І так продовжується до тих пір
    поки ви не витягнете усіх елементів із сервера. В такому випадку кнопка
    “Load More...” зникає."""
    
    --------------------------------------------------------------------
    
    	modified:   students/templates/students/pagination.html
    	modified:   students/templates/students/students_list.html
    	modified:   students/views/students.py
    	modified:   studentsdb/urls.py

--- students/views/students.py
+++ students/views/students.py
@@ -1,29 +1,68 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from ..models import Student
 
 # Views for Students
 
-def students_list(request):
-    students = (
-        {'id': 1,
-         'first_name': 'Андрій',
-         'last_name': 'Сорочан',
-         'ticket': 2123,
-         'image': 'img/my_photo.jpg'},
-        {'id': 2,
-         'first_name': 'Андрій2',
-         'last_name': 'Сорочан2',
-         'ticket': 21232,
-         'image': 'img/my_photo2.jpg'},
-        {'id': 3,
-         'first_name': 'Андрій3',
-         'last_name': 'Сорочан3',
-         'ticket': 21233,
-         'image': 'img/my_photo3.jpg'},
-)
+def students_list(request): 
+    students = Student.objects.all()
 
-    return render(request,'students/students_list.html',{'students':students})
+    if request.get_full_path() == "/":
+        #redirect request.GET on its copy(deep copy) which I will amend
+        request.GET = request.GET.copy()
+        #assign 'order_by' value 'last_name' 
+        request.GET.__setitem__('order_by', 'last_name')
+        #assign 'cadre' value '1' 
+        request.GET.__setitem__('cadre', '1')
+        request.GET.__setitem__('page', '1')
+ 
+    #try to order students list
+    order_by = request.GET.get('order_by', '')
+
+    if request.GET.get('order_by', '') in ('first_name', 'last_name', 'ticket'):
+        students = students.order_by(order_by)
+        if request.GET.get('reverse','') == '1':
+            students = students.reverse()
+ 
+   
+    #paginate students
+    num_students_in_one_page = 3
+    num_pages_in_on_cadre = 2
+    num_students_in_one_cadre = num_students_in_one_page * num_pages_in_on_cadre
+
+    paginator_inner  = Paginator(students, num_students_in_one_cadre)   # Students divided up cadre
+    page = request.GET.get('page')
+    cadre = request.GET.get('cadre')
+
+    try:
+        cadres_set = paginator_inner.page(cadre)                        # Cadre number='cadre' 
+        paginator = Paginator(cadres_set, num_students_in_one_page)     # Set of pages of students, show 3 students per page 
+        students = paginator.page(page)
+    except PageNotAnInteger:
+        # If page and cadre  are not an integer, deliver first page.
+        cadre = 1
+        cadres_set = paginator_inner.page(cadre)                        # First cadre
+        # Set of pages of students for first cadre, show 3 students per page
+        paginator = Paginator(cadres_set, num_students_in_one_page)       
+        students = paginator.page(1)                                    #first page of students from first cadre
+    except EmptyPage:
+        # If page and cadre are  out of range (e.g. 9999), deliver last page of results.
+        cadre = paginator_inner.num_pages
+        cadres_set = paginator_inner.page(cadre)
+        paginator = Paginator(paginator_inner.page(paginator_inner.num_pages), num_students_in_one_page) #last cadre
+        students = paginator.page(paginator.num_pages)                   #last student from last cadre
+
+    #Do range of numeration of pages in each cadre
+    page_start_index = 2*(int(cadre)-1)+1
+    page_end_index = 2*(int(cadre)-1)+students.paginator.num_pages
+    page_range_in_cadre = range(1,students.paginator.num_pages+1)      # Range of pages numbers only for one cadre
+    page_range_in_document = range(page_start_index,page_end_index+1)  # Range of pages numbers for whole document
+    combined_page_rage = [{'num_in_cadre':i,'num_in_document':k} for i,k in zip(page_range_in_cadre,page_range_in_document)] 
+    return render(request,'students/students_list.html',{'students':students, 'cadres_set':cadres_set,
+                          'combined_page_rage':combined_page_rage})
+
 
 def students_add(reduest):
     return HttpResponse('<h1>Student Add Form</h1>')
