dfe37e299b90f421a9dc9226acd0d55a209f2ed3
Andriy Sorochan <xai1983kbu@gmail.com>
2016-08-31 01:44:33 +0300

    Завершив етап ручного створення форми. Не вийшло обмежити її максимальну ширину: в css це обмеження прописав, але чомусь його не підхватило. ------------------------------------------------------------------------- 	modified:   students/static/css/main.css 	modified:   students/templates/students/base.html 	modified:   students/templates/students/forms/students_add.html 	modified:   students/views/students_add.py

--- students/views/students_add.py
+++ students/views/students_add.py
@@ -4,36 +4,76 @@ from django.http import HttpResponse, HttpResponseRedirect
 from students.models.students import Student
 from students.models.groups import Group
 from django.core.urlresolvers import reverse
-
+from datetime import datetime
 
 def students_add(request):
     # Якщо форма була запощена:
     # was form posted?
     if request.method == "POST":
-        # Якщо кнопка Скасувати була натиснута:
+        # Якщо кнопка Додати була натиснута:
         # was form add button clicked?
         if request.POST.get('add_button') is not None:
-
+            data = {'middle_name':request.POST['middle_name'], 
+                    'notes': request.POST['notes']}
             # Перевіряємо дані на коректність та збираємо помилки
-            # TODO: validate input from user
+            # validate input from user
             errors = {}
 
+            first_name = request.POST.get('first_name','').strip()
+            if not first_name:
+                errors['first_name'] = "Ім'я є обов'язковим"
+            else: 
+                data['first_name'] = first_name
+
+            last_name = request.POST.get('last_name','').strip()
+            if not last_name:
+                errors['last_name'] = "Прізвище є обов'язковим"
+            else: 
+                data['last_name'] = last_name
+
+            ticket = request.POST.get('ticket','').strip()
+            if not ticket:
+                errors['ticket'] = "Квиток є обов'язковим"
+            else: 
+                data['ticket'] = ticket
+
+            birthday = request.POST.get('birthday','').strip()
+            if not birthday:
+                errors['birthday'] = "Дата народження є обов'язковою"
+            else:
+                try:
+                    datetime.strptime(birthday,'%Y-%m-%d')
+                except Exception:
+                    errors['birthday'] = \
+                        "Введіть коректний формат дати (напр. 1984-12-30)"
+                else:
+                    data['birthday'] = birthday
+
+            student_group = request.POST['student_group']
+            if not student_group:
+                errors['student_group'] = "Група є обов'язковою"
+            else: 
+                groups = Group.objects.filter(pk=student_group)
+                if len(groups) != 1:
+                    errors['student_group'] = "Оберіть коректну групу" 
+                else:
+                    data['student_group'] = groups[0]
+
+            photo = request.FILES.get('photo')
+            if photo:
+                data['photo'] = photo 
+
+            #зберігаємо студента
             # Якщо дані були введені коректно:
             if not errors:
                 # Створюємо та зберігаємо студента в базу
                 #create student object
-                student = Student(first_name=request.POST['first_name'],
-                                  last_name=request.POST['last_name'],
-                                  middle_name=request.POST['middle_name'],
-                                  birthday=request.POST['birthday'],
-                                  ticket=request.POST['ticket'],
-                                  student_group=Group.objects.get(pk=request.POST['student_group']),
-                                  photo=request.FILES['photo'],)
+                student = Student(**data)
                 student.save()
-
                 # Повертаємо користувача до списку студентів
                 # redirect user to students_list
-                return HttpResponseRedirect(reverse('home'))
+                return HttpResponseRedirect('%s?status_message=Студента успішно додано!' %
+reverse('home'))
             # Якщо дані були введені некоректно:
             else:
                 # Віддаємо шаблон форми разом із знайденими помилками
@@ -41,12 +81,13 @@ def students_add(request):
                 return render(request,'students/forms/students_add.html',
                                       {'groups':Group.objects.all().order_by('title'),
                                        'errors':errors})
-        # Якщо форма не була запощена:
+        # Якщо кнопка Скасувати була натиснута:
         elif request.POST.get('cancel_button') is not None:
+            #Повертаємо користувача до списку студентів
             #redirect to home page on cancel button
-            print('cancel')
-            return HttpResponseRedirect(reverse('home'))
-            
+            return HttpResponseRedirect('%s?status_message=Додавання студента скасовано!' %
+reverse('home'))
+    # Якщо форма не була запощена:       
     else:
         # initial form render
         # повертаємо код початкового стану форми
